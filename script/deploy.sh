90f9beb1d307b0048bff9a4185b566c30b5c773a
sjackson142 <steven_c_jackson@hotmail.com>
2021-06-15 23:06:16 -0400

    Publish to docker hub

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 echo "Executing deploy.sh"
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
+docker tag $DOCKER_LOGIN/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
