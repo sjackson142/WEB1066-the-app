5dba99318fb49c9a5e0c95261fd0c6d51ff71d28
sjackson142 <steven_c_jackson@hotmail.com>
2021-06-16 00:13:26 -0400

    publish to docker hub

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 echo "Executing deploy.sh"
-bash -c  "echo \"${DOCKER_PASSWORD}\" | docker login --username ${DOCKER_LOGIN} --password-stdin"
-./gradlew -p ./monolithic/ui packageToContainer
-docker tag $DOCKER_LOGIN/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
+docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
